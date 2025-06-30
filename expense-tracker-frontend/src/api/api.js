7358ec9e26ee473aa987d496c3b7c90bb700522d
nakulpatel18 <157940594+nakulpatel18@users.noreply.github.com>
2025-06-30 22:39:29 +0530

    Update API base URL for deployed

--- expense-tracker-frontend/src/api/api.js
+++ expense-tracker-frontend/src/api/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: '/api', // Adjust this if your backend is on a different URL
+    baseURL: 'https://expensetracker-pi-five.vercel.app//api', // Adjust this if your backend is on a different URL
+    headers: {
+        'Content-Type': 'application/json',
+    },
 });
 
 // Add a request interceptor to add the auth token to every request
@@ -10,6 +13,7 @@ api.interceptors.request.use(
         const token = localStorage.getItem('token');
         if (token) {
             config.headers.Authorization = `Bearer ${token}`;
+            config.headers['x-auth-token'] = token;
         }
         return config;
     },
